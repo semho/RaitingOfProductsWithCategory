7d71cc7875457181fdb040fa1836a593fdb0d6be
unknown <semho@yandex.ru>
2023-01-07 20:17:54 +0300

    added configModule

--- src/top-page/top-page.controller.ts
+++ src/top-page/top-page.controller.ts
@@ -8,16 +8,21 @@ import {
   Patch,
   Post,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config/dist';
 import { FindTopPageDto } from './dto/find-top-page.dto';
 import { TopPageModel } from './top-page.model';
 
 @Controller('top-page')
 export class TopPageController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Post('create')
   async create(@Body() dto: Omit<TopPageModel, '_id'>) {}
 
   @Get(':id')
-  async get(@Param('id') id: string) {}
+  async get(@Param('id') id: string) {
+    this.configService.get<string>('TEST');
+  }
 
   @Delete(':id')
   async delete(@Param('id') id: string) {}
--- src/top-page/top-page.module.ts
+++ src/top-page/top-page.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config/dist';
 import { TopPageController } from './top-page.controller';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [TopPageController],
 })
 export class TopPageModule {}
