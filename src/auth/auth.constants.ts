e30c42f74f8c5f7bffc2723519fd024b6da5a3b5
Semyon Khokhalev <semho@yandex.ru>
2023-01-25 06:52:21 +0300

    added jwt

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,2 +1,5 @@
 export const ALREADY_REGISTERED_ERROR =
   'Такой пользователь уже зарегистрирован';
+
+export const USER_NOT_FOUND_ERROR = 'Пользователь с таким email не найдет';
+export const WRONG_PASSWORD_ERROR = 'Пароль не верный';
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -22,7 +22,12 @@ export class AuthController {
     return this.authService.createUser(dto);
   }
 
+  @UsePipes(new ValidationPipe())
   @HttpCode(200)
   @Post('login')
-  async login(@Body() dto: AuthDto) {}
+  async login(@Body() { login, password }: AuthDto) {
+    const { email } = await this.authService.validateUser(login, password);
+
+    return this.authService.login(email);
+  }
 }
